16e62cf5c5e79af6456a88fe057a1404179f937a
patrickstinson <patrickstinson@gmail.com>
2026-09-10 14:39:20 -0400

    edit tests

--- tests/test_connected_edge_drop.py
+++ tests/test_connected_edge_drop.py
@@ -1,6 +1,7 @@
 import numpy as np
 import torch
 from torch import Tensor
+import pytest
 
 from connected_edge_dropout import ConnectedEdgeDropout
 from .sbm import sbm
@@ -20,15 +21,11 @@ def num_components(edge_index, directed):
         return connected_components(A, directed=True, connection='weak')[0]
 
 
-if __name__ == "__main__":
-    rng = np.random.default_rng(0)
-    for mixing in (0.001, 0.002, 0.005, 0.01, 0.02, 0.05, 0.1, 0.3):
-        ei, blocks = sbm(3000, 6, mean_degree=8.0, mixing=mixing, rng=rng, connect="repair")
-        for p in (0, .1, .25, .5, .75, 1.):
-            sed = ConnectedEdgeDropout(torch.tensor(ei), p)
-            surviving_edges = sed()[0]
-            if not num_components(surviving_edges, directed=False) == 1:
-                raise Exception("Failed to produce a connected graph.")
-    print('Passed all tests.')
-
-
+rng = np.random.default_rng(0)
+@pytest.mark.parametrize("mixing", (0.001, 0.002, 0.005, 0.01, 0.02, 0.05, 0.1, 0.3))
+@pytest.mark.parametrize("p", (0, .1, .25, .5, .75, 1.))
+def test_remains_connected(mixing, p):
+    ei, blocks = sbm(3000, 6, mean_degree=8.0, mixing=mixing, rng=rng, connect="repair")
+    sed = ConnectedEdgeDropout(torch.tensor(ei), p)
+    surviving_edges = sed()[0]
+    assert num_components(surviving_edges, directed=False) == 1
